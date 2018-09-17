3afbd7c88f554004e42ae9748d266541aa3d3dfb
kairi <kairi@kairis-MacBook-Pro.local>
2018-09-17 12:49:17 -0400

    tab bar setup and collection view setup added

---
+++ flix_app/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_app
+//
+//  Created by kairi on 9/17/18.
+//  Copyright © 2018 kairi. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
