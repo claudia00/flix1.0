065110bb9bfd9b2a1611f9ad59772eda413d8290
kairi <kairi@kairis-MacBook-Pro.local>
2018-09-26 23:32:48 -0400

    auto layout lesson

--- flix_app/Views/MovieCell.swift
+++ flix_app/Views/MovieCell.swift
@@ -18,8 +18,8 @@ class MovieCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
-
+            }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
